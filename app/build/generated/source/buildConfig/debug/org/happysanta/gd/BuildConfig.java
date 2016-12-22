bd932ebddbb6ea83be606d878cceada3e5abbf11
SnowVolf <aftervolf@gmail.com>
2016-12-22 22:40:11 +0700

    initial commit - gradle migration

---
+++ app/build/generated/source/buildConfig/debug/org/happysanta/gd/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.happysanta.gd;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.happysanta.gd";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 20161223;
+  public static final String VERSION_NAME = "2.0.20161223";
+}
